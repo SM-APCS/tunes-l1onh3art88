3684821071efeae0b446b1520b889bc4683c0ad2
l1onh3art88 <spenccheng@gmail.com>
2015-12-17 06:42:28 -0600

    Tunes

--- Tunes/src/tunes/Tunes.java
+++ Tunes/src/tunes/Tunes.java
@@ -12,6 +12,19 @@ public class Tunes {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        CDCollection music = new CDCollection();
+        
+        music.addCD("By the Way", "Red Hot Chili Peppers", 14.95, 10);
+        music.addCD("Come On Over", "Shania Twain", 14.95, 16);
+        music.addCD("Soundtrack", "The Producers", 17.95, 33);
+        music.addCD("Play", "Jennifer Lopez", 13.90, 11);
+        
+        System.out.println(music);
+        
+        music.addCD("Double Live","Garth Brooks",19.99, 26);
+        music.addCD("Greatest Hits", "Stone Temple Pilots", 15.95, 13);
+        
+        System.out.println(music);
     }
     
 }
