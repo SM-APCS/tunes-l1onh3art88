d588a5381f58c85a8f168cb11be2bf59513b9091
l1onh3art88 <spenccheng@gmail.com>
2015-12-17 06:42:22 -0600

    Tunes

--- Tunes/src/tunes/CD.java
+++ Tunes/src/tunes/CD.java
@@ -3,9 +3,32 @@ package tunes;
 
 /**
  * AP Computer Science
- * @author Mr. Tholking
+ * @author Spencer Cheng
  * 
  */
+import java.text.NumberFormat;
 public class CD {
-
+    private String title, artist;
+    private double cost;
+    private int tracks;
+    
+    //Creates a new CD with the specified information
+    
+    public CD(String name, String singer, double price, int numTracks){
+        title = name;
+        artist = singer;
+        cost = price;
+        tracks = numTracks;
+    }
+    //Returns a description of this CD.
+    
+    public String toString(){
+        NumberFormat fmt=NumberFormat.getCurrencyInstance();
+        
+        String description;
+        description = fmt.format(cost) + "\t" + tracks + "\t";
+        description += title + "\t" + artist;
+        
+        return description;
+    }
 }
